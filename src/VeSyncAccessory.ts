6a5408aa44f969ff576a6ba72171e851693836ec
RaresAil <ailincairares1@gmail.com>
2021-11-17 23:51:00 +0200

    feat(better-error-handling)

--- src/VeSyncAccessory.ts
+++ src/VeSyncAccessory.ts
@@ -19,8 +19,8 @@ export type AccessoryThisType = ThisType<{
 }>;
 
 export default class VeSyncAccessory {
-  private airQualitySensorService: Service;
-  private airPurifierService: Service;
+  private airQualitySensorService?: Service;
+  private airPurifierService?: Service;
 
   public get UUID() {
     return this.device.uuid.toString();
@@ -34,70 +34,74 @@ export default class VeSyncAccessory {
     private readonly platform: Platform,
     private readonly accessory: VeSyncPlatformAccessory
   ) {
-    const { manufacturer, model, mac } = this.device;
+    try {
+      const { manufacturer, model, mac } = this.device;
 
-    this.accessory
-      .getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(
-        this.platform.Characteristic.Manufacturer,
-        manufacturer
-      )
-      .setCharacteristic(this.platform.Characteristic.Model, model)
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, mac);
+      this.accessory
+        .getService(this.platform.Service.AccessoryInformation)!
+        .setCharacteristic(
+          this.platform.Characteristic.Manufacturer,
+          manufacturer
+        )
+        .setCharacteristic(this.platform.Characteristic.Model, model)
+        .setCharacteristic(this.platform.Characteristic.SerialNumber, mac);
 
-    this.airPurifierService =
-      this.accessory.getService(this.platform.Service.AirPurifier) ||
-      this.accessory.addService(this.platform.Service.AirPurifier);
+      this.airPurifierService =
+        this.accessory.getService(this.platform.Service.AirPurifier) ||
+        this.accessory.addService(this.platform.Service.AirPurifier);
 
-    this.airQualitySensorService =
-      this.accessory.getService(this.platform.Service.AirQualitySensor) ||
-      this.accessory.addService(this.platform.Service.AirQualitySensor);
+      this.airQualitySensorService =
+        this.accessory.getService(this.platform.Service.AirQualitySensor) ||
+        this.accessory.addService(this.platform.Service.AirQualitySensor);
 
-    this.airPurifierService.setPrimaryService(true);
-    this.airPurifierService.addLinkedService(this.airQualitySensorService);
+      this.airPurifierService.setPrimaryService(true);
+      this.airPurifierService.addLinkedService(this.airQualitySensorService);
 
-    this.airPurifierService
-      .getCharacteristic(this.platform.Characteristic.Active)
-      .onGet(Active.get.bind(this))
-      .onSet(Active.set.bind(this));
+      this.airPurifierService
+        .getCharacteristic(this.platform.Characteristic.Active)
+        .onGet(Active.get.bind(this))
+        .onSet(Active.set.bind(this));
 
-    this.airPurifierService
-      .getCharacteristic(this.platform.Characteristic.CurrentAirPurifierState)
-      .onGet(CurrentState.get.bind(this));
+      this.airPurifierService
+        .getCharacteristic(this.platform.Characteristic.CurrentAirPurifierState)
+        .onGet(CurrentState.get.bind(this));
 
-    this.airPurifierService
-      .getCharacteristic(this.platform.Characteristic.TargetAirPurifierState)
-      .onGet(TargetState.get.bind(this))
-      .onSet(TargetState.set.bind(this));
+      this.airPurifierService
+        .getCharacteristic(this.platform.Characteristic.TargetAirPurifierState)
+        .onGet(TargetState.get.bind(this))
+        .onSet(TargetState.set.bind(this));
 
-    this.airPurifierService
-      .getCharacteristic(this.platform.Characteristic.LockPhysicalControls)
-      .onGet(LockPhysicalControls.get.bind(this))
-      .onSet(LockPhysicalControls.set.bind(this));
+      this.airPurifierService
+        .getCharacteristic(this.platform.Characteristic.LockPhysicalControls)
+        .onGet(LockPhysicalControls.get.bind(this))
+        .onSet(LockPhysicalControls.set.bind(this));
 
-    this.airPurifierService
-      .getCharacteristic(this.platform.Characteristic.RotationSpeed)
-      .setProps({
-        minStep: this.device.deviceType.speedMinStep,
-        maxValue: 100
-      })
-      .onGet(RotationSpeed.get.bind(this))
-      .onSet(RotationSpeed.set.bind(this));
+      this.airPurifierService
+        .getCharacteristic(this.platform.Characteristic.RotationSpeed)
+        .setProps({
+          minStep: this.device.deviceType.speedMinStep,
+          maxValue: 100
+        })
+        .onGet(RotationSpeed.get.bind(this))
+        .onSet(RotationSpeed.set.bind(this));
 
-    this.airQualitySensorService
-      .getCharacteristic(this.platform.Characteristic.AirQuality)
-      .onGet(AirQuality.get.bind(this));
+      this.airQualitySensorService
+        .getCharacteristic(this.platform.Characteristic.AirQuality)
+        .onGet(AirQuality.get.bind(this));
 
-    this.airQualitySensorService
-      .getCharacteristic(this.platform.Characteristic.PM2_5Density)
-      .onGet(PM25Density.get.bind(this));
+      this.airQualitySensorService
+        .getCharacteristic(this.platform.Characteristic.PM2_5Density)
+        .onGet(PM25Density.get.bind(this));
 
-    this.airPurifierService
-      .getCharacteristic(this.platform.Characteristic.FilterChangeIndication)
-      .onGet(FilterChangeIndication.get.bind(this));
+      this.airPurifierService
+        .getCharacteristic(this.platform.Characteristic.FilterChangeIndication)
+        .onGet(FilterChangeIndication.get.bind(this));
 
-    this.airPurifierService
-      .getCharacteristic(this.platform.Characteristic.FilterLifeLevel)
-      .onGet(FilterLifeLevel.get.bind(this));
+      this.airPurifierService
+        .getCharacteristic(this.platform.Characteristic.FilterLifeLevel)
+        .onGet(FilterLifeLevel.get.bind(this));
+    } catch (error: any) {
+      this.platform.log.error(`Error: ${error?.message}`);
+    }
   }
 }
