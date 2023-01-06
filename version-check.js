505d8c3f6889ce6665daa2f0bb57277dca2efb2f
RaresAil <ailincairares1@gmail.com>
2023-01-06 15:23:35 +0200

    feat/load-everest

--- version-check.js
+++ version-check.js
@@ -1,8 +1,12 @@
-const fs = require('fs');
-const package = require('./package.json');
+const fs = require("fs");
+const package = require("./package.json");
 
-console.log('Version: %o', package.version);
+console.log("Version: %o", package.version);
 
-if (package.version.includes('-rc')) {
-    fs.writeFileSync('.prerelease', 'NPM_EX="--tag prerelease"', 'utf8');
+if (package.version.includes("-rc")) {
+  const name = package.version
+    .split("-rc")[1]
+    .slice(package.version.indexOf(".") + 1, package.version.length);
+
+  fs.writeFileSync(".prerelease", `NPM_EX="--tag prerelease-${name}"`, "utf8");
 }
