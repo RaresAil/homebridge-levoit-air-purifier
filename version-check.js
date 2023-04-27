99c44712fabfa162ee377a772ee030e834b48b21
RaresAil <ailincairares1@gmail.com>
2023-04-27 15:01:29 +0300

    feat/fix-publish

--- version-check.js
+++ version-check.js
@@ -3,6 +3,10 @@ const package = require('./package.json');
 
 console.log('Version: %o', package.version);
 
-if (package.version.includes('-rc')) {
-    fs.writeFileSync('.prerelease', 'NPM_EX="--tag prerelease"', 'utf8');
+if (package.version.includes("-rc")) {
+  const name = package.version
+    .split("-rc")[1]
+    .slice(package.version.indexOf(".") + 1, package.version.length);
+
+  fs.writeFileSync(".prerelease", `NPM_EX=--tag prerelease-${name}`, "utf8");
 }
